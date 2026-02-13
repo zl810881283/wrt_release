847e1425bbd9d2d92c89b72d7ddcabb21dd98efa
zl <810881283@qq.com>
2026-02-14 01:48:21 +0800

    更新 argon

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -78,7 +78,7 @@ install_small8() {
         luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest netdata luci-app-netdata \
         lucky luci-app-lucky luci-app-openclash luci-app-homeproxy luci-app-amlogic nikki luci-app-nikki \
         tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
-        msd_lite luci-app-msd_lite cups luci-app-cupsd
+        msd_lite luci-app-msd_lite cups luci-app-cupsd luci-theme-argon luci-app-argon-config
 }
 
 install_passwall() {
